e00979ef76f1d2848e76350f936f9fad5c5c459f
intramigo <intramigo@gmail.com>
2012-11-16 21:30:03 -0600

    removed some stuff

--- old_youtube_vid_stuff_and_other/pages/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class HomePage(models.Model):
-    homecopy    = models.TextField()   # so this holds the text you entered via admin interface
-
-    def __unicode__(self):
-        return 'Home Page Copy'  # (every instance of) this model calls itself 'Home Page Copy'... but we'll only have one instance of this so that's ok
